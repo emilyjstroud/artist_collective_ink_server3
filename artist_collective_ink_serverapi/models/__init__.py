5cd73edeb4949d9f565306405fb3bbc2113bad07
Emily Stroud <emilystroud93@gmail.com>
2023-01-11 20:58:48 -0600

    added user

--- artist_collective_ink_serverapi/models/__init__.py
+++ artist_collective_ink_serverapi/models/__init__.py
@@ -2,3 +2,4 @@ from .artist import Artist
 from .shop import Shop
 from .style import Style
 from .artist_style import ArtistStyle
+from .user import User
---
+++ artist_collective_ink_serverapi/views/__init__.py
@@ -0,0 +1,6 @@
+from .auth import check_user, register_user
+from .shop import ShopView
+from .artist import ArtistView
+from .style import StyleView
+from .artist_style import ArtistStyleView
+from .user import UserView
